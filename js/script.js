27e7f632975de31f6f2425458b8624df795c20ca
TK <149270184+baby-got-stack@users.noreply.github.com>
2023-11-24 17:34:59 -0500

    Complete fully functioning app with addition of UI
    Finish game with all functionality and completed UI.

--- js/script.js
+++ js/script.js
@@ -1,151 +1,111 @@
-// generate randomized computer choice of 'rock', 'paper', or 'scissors'
-// get user's choice and save it
-// check computer's choice against user's choice
-// return the winner
-// generate a new computer choice
-// get user's new choice and save it
-// compare computer and user choices again
-// at the end of five rounds, determine who has won most
-// return the overall winner of the five round match
+let rockBtn = document.querySelector('#rock');
+let paperBtn =  document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
+let userScoreText = document.querySelector('.your-score');
+let computerScoreText = document.querySelector('.computer-score');
+let roundResultText = document.querySelector('.round-result');
+let playAgainBtn = document.querySelector('.play-again-button');
+let gameOverText = document.querySelector('.winner');
+let userWins = 0;
+let computerWins = 0;
 
-let playerSelection = 'rock';
-let computerSelection = getComputerChoice();
+userScoreText.textContent = `Your score: ${userWins}`;
+computerScoreText.textContent = `Computer score: ${computerWins}`;
+
+rockBtn.addEventListener('click', () => {
+    playGame('Rock', getComputerChoice());
+    checkGameOver();
+});
+
+paperBtn.addEventListener('click', () => {
+    playGame('Paper', getComputerChoice());
+    checkGameOver();
+});
+
+scissorsBtn.addEventListener('click', () => {
+    playGame('Scissors', getComputerChoice());
+    checkGameOver();
+});
+
+playAgainBtn.addEventListener('click', () => {
+    resetGame();
+});
+
+function checkGameOver(){
+    if (userWins === 5 || computerWins === 5){
+        playAgainBtn.classList.remove('hidden');
+        rockBtn.style.pointerEvents = 'none';
+        paperBtn.style.pointerEvents = 'none';
+        scissorsBtn.style.pointerEvents = 'none';
+
+        if(userWins === 5){
+            gameOverText.textContent = 'Game over! The user is the winner!';
+        } else{
+            gameOverText.textContent = 'Game over! The computer is the winner!';
+        }
+    }
+}
+
+function resetGame(){
+    userWins = 0;
+    computerWins = 0;
+    userScoreText.textContent = `Your score: ${userWins}`;
+    computerScoreText.textContent = `Computer score: ${userWins}`;
+    roundResultText.textContent = '';
+    gameOverText.textContent = '';
+    playAgainBtn.classList.add('hidden');
+    rockBtn.style.pointerEvents = 'auto';
+    paperBtn.style.pointerEvents = 'auto';
+    scissorsBtn.style.pointerEvents = 'auto';
+}
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
-    let computerChoice = 'default string';
-
-    if (randomNumber <= 3) {
-        computerChoice = 'scissors';
-    } else if (randomNumber <= 6) {
-        computerChoice = 'paper';
-    } else {
-        computerChoice = 'rock';
-    }
-    computerChoice = computerChoice.toLowerCase();
-    return computerChoice;
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    computerPick = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerPick)
+    return computerPick;
 }
 
-function playOneRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    let endOfGameMessage = 'The game is over. Who won?';
+function playGame(playerSelection, computerSelection){
+    let endOfGameMessage = '';
 
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         endOfGameMessage = 'You win! Rock beats Scissors.';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        roundResultText.textContent = 'You win! Rock beats Scissors.';
+        userWins++;
+        userScoreText.textContent = `Your score: ${userWins}`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         endOfGameMessage = 'You win! Paper beats Rock.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        roundResultText.textContent = 'You win! Paper beats Rock.';
+        userWins++;
+        userScoreText.textContent = `Your score: ${userWins}`;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         endOfGameMessage = 'You win! Scissors beats Paper.';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        roundResultText.textContent = 'You win! Scissors beats Paper.';
+        userWins++;
+        userScoreText.textContent = `Your score: ${userWins}`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         endOfGameMessage = 'You lose! Paper beats Rock.';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        roundResultText.textContent = 'You lose! Paper beats Rock.';
+        computerWins++;
+        computerScoreText.textContent = `Computer score: ${computerWins}`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         endOfGameMessage = 'You lose! Scissors beats Paper.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        roundResultText.textContent = 'You lose! Scissors beats Paper.';
+        computerWins++;
+        computerScoreText.textContent = `Computer score: ${computerWins}`;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         endOfGameMessage = 'You lose! Rock beats Scissors.';
+        roundResultText.textContent = 'You lose! Rock beats Scissors.';
+        computerWins++;
+        computerScoreText.textContent = `Computer score: ${computerWins}`;
     } else if (playerSelection === computerSelection) {
-        endOfGameMessage = `It's a tie!`;
+        endOfGameMessage = `It's a tie! ${playerSelection} ties ${computerSelection}.`;
+        roundResultText.textContent = `It's a tie! ${playerSelection} ties ${computerSelection}.`;
     } else {
-        endOfGameMessage = 'You entered an invalid option. Please enter Rock, Paper, or Scissors.';
+        endOfGameMessage = 'Something went wrong...';
     }
 
-    return endOfGameMessage;
+    // Just to see the results clearly laid out in the console
+    console.log(endOfGameMessage);
 }
-
-function game() {
-    let userWins = 0;
-    let computerWins = 0;
-    let grandWinner = 'We do not know who wil win yet';
-    computerSelection = getComputerChoice();
-    playerSelection = prompt();
-
-    playOneRound(playerSelection, computerSelection);
-    if (playOneRound(playerSelection, computerSelection) === 'You win! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You win! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You win! Scissors beats Paper.') {
-        userWins++;
-        console.log('The user won the first round.');
-    } else if (playOneRound(playerSelection, computerSelection) === `It's a tie!`) {
-        console.log('The first round was a tie.');
-    } else if (playOneRound(playerSelection, computerSelection) === 'You lose! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You lose! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You lose! Scissors beats Paper.') {
-        computerWins++;
-        console.log('The computer won the first round.');
-    } else {
-        console.log('You entered an invalid option. Please try again.');
-    }
-
-    computerSelection = getComputerChoice();
-    playerSelection = prompt();
-
-    playOneRound(playerSelection, computerSelection);
-    if (playOneRound(playerSelection, computerSelection) === 'You win! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You win! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You win! Scissors beats Paper.') {
-        userWins++;
-        console.log('The user won the second round.');
-    } else if (playOneRound(playerSelection, computerSelection) === `It's a tie!`) {
-        console.log('The second round was a tie.');
-    } else if (playOneRound(playerSelection, computerSelection) === 'You lose! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You lose! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You lose! Scissors beats Paper.') {
-        computerWins++;
-        console.log('The computer won the second round.');
-    } else {
-        console.log('You entered an invalid option. Please try again.');
-    }
-
-    computerSelection = getComputerChoice();
-    playerSelection = prompt();
-
-    playOneRound(playerSelection, computerSelection);
-    if (playOneRound(playerSelection, computerSelection) === 'You win! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You win! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You win! Scissors beats Paper.') {
-        userWins++;
-        console.log('The user won the third round.');
-    } else if (playOneRound(playerSelection, computerSelection) === `It's a tie!`) {
-        console.log('The second third was a tie.');
-    } else if (playOneRound(playerSelection, computerSelection) === 'You lose! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You lose! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You lose! Scissors beats Paper.') {
-        computerWins++;
-        console.log('The computer won the third round.');
-    } else {
-        console.log('You entered an invalid option. Please try again.');
-    }
-
-    computerSelection = getComputerChoice();
-    playerSelection = prompt();
-
-    playOneRound(playerSelection, computerSelection);
-    if (playOneRound(playerSelection, computerSelection) === 'You win! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You win! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You win! Scissors beats Paper.') {
-        userWins++;
-        console.log('The user won the fourth round.');
-    } else if (playOneRound(playerSelection, computerSelection) === `It's a tie!`) {
-        console.log('The fourth round was a tie.');
-    } else if (playOneRound(playerSelection, computerSelection) === 'You lose! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You lose! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You lose! Scissors beats Paper.') {
-        computerWins++;
-        console.log('The computer won the fourth round.');
-    } else {
-        console.log('You entered an invalid option. Please try again.');
-    }
-
-    computerSelection = getComputerChoice();
-    playerSelection = prompt();
-
-    playOneRound(playerSelection, computerSelection);
-    if (playOneRound(playerSelection, computerSelection) === 'You win! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You win! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You win! Scissors beats Paper.') {
-        userWins++;
-        console.log('The user won the fifth round.');
-    } else if (playOneRound(playerSelection, computerSelection) === `It's a tie!`) {
-        console.log('The fifth round was a tie.');
-    } else if (playOneRound(playerSelection, computerSelection) === 'You lose! Rock beats Scissors.' || playOneRound(playerSelection, computerSelection) === 'You lose! Paper beats Rock.' || playOneRound(playerSelection, computerSelection) === 'You lose! Scissors beats Paper.') {
-        computerWins++;
-        console.log('The computer won the fifth round.');
-    } else {
-        console.log('You entered an invalid option. Please try again.');
-    }
-
-    if (userWins > computerWins) {
-        grandWinner = `The user is the grand winner! The user won ${userWins} times out of the 5 rounds.`;
-    } else if (computerWins > userWins) {
-        grandWinner = `The computer is the grand winner! The computer won ${computerWins} times out of the 5 rounds.`;
-    } else if (userWins = computerWins) {
-        grandWinner = `There was a tie! The user won ${userWins} times out of the 5 rounds and the computer won ${computerWins} times out of the five rounds.`
-    } else {
-        grandWinner = 'An invalid input was entered.'
-    }
-
-    return grandWinner;
-}
-
-console.log(game());
